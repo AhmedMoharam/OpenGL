96410037593c2f3408d181a3f5662f3ad330644a
AhmedMoharam <33280932+AhmedMoharam@users.noreply.github.com>
2022-12-04 10:53:55 +0100

    keyboard demo
    do something when glfw key is pressed

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -6,6 +6,36 @@
 #include "Shader.h"
 
 
+
+#include <functional>
+
+void do_smthng() {
+	// shader update position
+	std::cout << "Key is pressed" << std::endl;
+}
+
+
+
+class InputControl {
+public:
+	void configure(int key, int action, std::function<void()> func){
+		m_key = key;
+		m_func = func;
+	}
+
+	std::function<void()> m_func;
+	int m_key;
+};
+
+InputControl inputs;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == inputs.m_key && action == GLFW_PRESS)
+		inputs.m_func();
+}
+
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -34,6 +64,9 @@ int main(void)
 		std::cout << "error occured!" << std::endl;
 		return 1;
 	}
+
+	/*setup keys Input*/
+	glfwSetKeyCallback(window, key_callback);
 	
 	float postions[] = { -0.5f, -0.5f,	//0
 						  0.5f , -0.5f,	//1
@@ -43,11 +76,16 @@ int main(void)
 	unsigned char indecies[] = { 0,1,2,	//1st triangle
 								 0,2,3 };	//2nd triangle
 	
+	// put the veticies data into GPU memory (vertix array buffer) - raw data
 	VertexArrayBuffer vb(postions, sizeof(float) * 2 * 4);
+	// create a layout for the data, descripe the raw data layout you entered -- this case each vertix is 2 floats
 	VertexAttribLayout layout;
 	layout.push<float>(2, false);
+	// now connect the vertix attribute layout and the vetrix array buffer with vertix array object
 	VertexArrayObject vao;
 	vao.AddLayout(layout, vb);
+
+	// put the indecies to index buffer
 	IndexBuffer ib(indecies, sizeof(unsigned char) * 2 * 3);
 	ShaderProgram program("Resources/Shaders/basic.shader");
 	
@@ -56,6 +94,7 @@ int main(void)
 	glProgramUniform4f(program.GetId(), u_color, 1.0f, 0.0f, 1.0f, 1.0f);
 
 	
+	inputs.configure((int)GLFW_KEY_E, (int)GLFW_PRESS, do_smthng);
 
 	vb.UnBind();
 	vao.Bind();
